2a3646520b2647d00822053b1bf191513e6bdd2b
Abey Onalaja <abey.onalaja@gmail.com>
2018-10-13 01:58:25 +0100

    Add Meta compoent

--- components/Page.js
+++ components/Page.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import Meta from "./Meta";
 
 class Page extends Component {
   render() {
     return (
       <div>
+        <Meta />
         <Header />
         <p>Hey i am the page component</p>
         {this.props.children}
